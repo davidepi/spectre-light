d72d6c05bf1b7100efcbcad1bc1c6a22f36578ea
Davide Pizzolotto <itspesa@gmail.com>
2017-04-30 23:54:51 +0200

    Useless main, just to check if everything works

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
+//Just for testing purposes
+#include "utility.hpp"
+#include "vec2.hpp"
+#include "vec3.hpp"
+#include "point3.hpp"
+#include "matrix4.hpp"
+#include "AABB.hpp"
 int main()
 {
+    Vec2 v2(0,1);
+    Vec3 v3(v2,-1);
+    Point3 p1(0,0,0);
+    Point3 p2(1,1,1);
+    Matrix4 m;
+    m.setIdentity();
+    AABB aabb(&p1,&p2);
     return 0;
 }
