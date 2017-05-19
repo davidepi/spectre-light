109acbbae7476ad11fc427e9d0c5ec4fcc925a3c
Davide Pizzolotto <itspesa@gmail.com>
2017-05-19 12:09:23 +0200

    WELL rng, rewritten to be thread safe

---
+++ src/utility/wellrng.cpp
@@ -0,0 +1,54 @@
+//Original code by:
+//      Francois Panneton and Pierre L'Ecuyer, University of Montreal
+//      Makoto Matsumoto, Hiroshima University
+
+//Edited because I need a thread safe version, thus I embedded everything into
+//a class
+
+#include "wellrng.hpp"
+
+WELLrng::WELLrng(unsigned int seed[WELL_R])
+{
+    WELLrng::state_i = 0;
+    for(int i=0;i<WELL_R;i++)
+    {
+        state[i] = seed[i];
+    }
+}
+
+float WELLrng::getNumberf()
+{
+    z0 = state[(state_i+31) & 0x0000001FU];
+    z1 = (state[state_i ]) ^ (state[(state_i+3) & 0x0000001FU] ^
+                              (state[(state_i+3) & 0x0000001FU]>>8));
+    z2 = (state[(state_i+24) & 0x0000001FU] ^
+          (state[(state_i+24) & 0x0000001FU]<<(-(-19)))) ^
+        (state[(state_i+10) & 0x0000001FU] ^
+         (state[(state_i+10) & 0x0000001FU]<<(-(-14))));
+    state[state_i ] = z1 ^ z2;
+    state[(state_i+31) & 0x0000001FU] = (z0^(z0<<(-(-11)))) ^
+    (z1^(z1<<(-(-7)))) ^ (z2^(z2<<(-(-13)))) ;
+    state_i = (state_i + 31) & 0x0000001FU;
+    return ((float) state[state_i] * 2.32830643653869628906e-10f);
+}
+
+double WELLrng::getNumber()
+{
+    z0 = state[(state_i+31) & 0x0000001FU];
+    z1 = (state[state_i ]) ^ (state[(state_i+3) & 0x0000001FU] ^
+                              (state[(state_i+3) & 0x0000001FU]>>8));
+    z2 = (state[(state_i+24) & 0x0000001FU] ^
+          (state[(state_i+24) & 0x0000001FU]<<(-(-19)))) ^
+    (state[(state_i+10) & 0x0000001FU] ^
+     (state[(state_i+10) & 0x0000001FU]<<(-(-14))));
+    state[state_i ] = z1 ^ z2;
+    state[(state_i+31) & 0x0000001FU] = (z0^(z0<<(-(-11)))) ^
+    (z1^(z1<<(-(-7)))) ^ (z2^(z2<<(-(-13)))) ;
+    state_i = (state_i + 31) & 0x0000001FU;
+    return ((double) state[state_i] * 2.32830643653869628906e-10f);
+}
+
+WELLrng::~WELLrng()
+{
+    
+}
---
+++ src/utility/wellrng.hpp
@@ -0,0 +1,70 @@
+//Created,  19 May 2017
+//Last Edit 19 May 2017
+
+/**
+ *  \file wellrng.hpp
+ *  \brief     Implements the WELL rng
+ *  \details   C++ implementation of the WELL pseudo random number generator,
+ *             originally published by Francois Panneton and Pierre L'Ecuyer
+ *  \version   0.1
+ *  \date      19 May 2017
+ */
+
+
+#ifndef __WELLRNG_HPP__
+#define __WELLRNG_HPP__
+
+///The number of states of the prng
+#define WELL_R 32
+
+/**
+ *  \class WELLrng wellrng.hpp "utility/wellrng.hpp"
+ *  \brief WELL pseudo random number generator
+ *
+ *  This class is a wrapper for the WELL pseudo random number generator
+ *  developed by Francois Panneton and Pierre L'Ecuyer. The init is embedded
+ *  into the class and the generator does not use global variable anymore, thus
+ *  it is now thread safe
+ */
+class WELLrng
+{
+public:
+    
+    /** \brief Default constructor
+     *
+     *  Initialize the prng engine with the given seed
+     *
+     *  \param[in] seed 128 bytes representing the seed of this prng
+     */
+    WELLrng(unsigned int seed[WELL_R]);
+    
+    ///Default destructor
+    ~WELLrng();
+    
+    /** \brief Generate a random float number
+     *
+     *  Generate a random number in the range [0.0,1.0] following an uniform
+     *  distribution
+     *
+     *  \return The generated number
+     */
+    float getNumberf();
+    
+    /** \brief Generate a random double number
+     *
+     *  Generate a random number in the range [0.0,1.0] following an uniform
+     *  distribution
+     *
+     *  \return The generated number
+     */
+    double getNumber();
+    
+private:
+    unsigned int state_i;
+    unsigned int state[WELL_R];
+    unsigned int z0;
+    unsigned int z1;
+    unsigned int z2;
+};
+
+#endif
