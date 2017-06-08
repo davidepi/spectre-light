28499560853007e03bd7330a8dcb9f7a7545e327
Davide Pizzolotto <itspesa@gmail.com>
2017-06-08 21:35:55 +0200

    Oren-Nayar reflection model

---
+++ src/materials/oren_nayar.cpp
@@ -0,0 +1,53 @@
+#include "oren_nayar.hpp"
+
+OrenNayar::OrenNayar(Color diffuse, float sigma) : Bdf(BdfFlags(BRDF|DIFFUSE))
+{
+    float sigma2 = sigma*sigma;
+    OrenNayar::A = 1.f - (sigma2/(2*(sigma2+0.33f)));
+    OrenNayar::B = (0.45f*sigma2)/(sigma2+0.09f);
+    OrenNayar::diffuse = diffuse;
+}
+
+OrenNayar::~OrenNayar()
+{
+
+}
+
+Color OrenNayar::df(const Vec3 *wout, const Vec3 *wincident) const
+{
+    float costhetain = wincident->z;
+    float costhetaout = wout->z;
+    float abscosthetain = fabsf(costhetain);
+    float abscosthetaout = fabsf(costhetaout);
+    float sinthetain = sqrtf(1.f-costhetain*costhetain);
+    float sinthetaout = sqrtf(1.f-costhetaout*costhetaout);
+
+    //cos(phiin - phiout) = cos(phiin)*cos(phiout)+sin(phiin)*sin(phiout)
+    float maxcos = 0.0f;
+    if(sinthetain>0 && sinthetaout>0) //cos(phiin - phiout) positive
+    {
+        float cosphiin, cosphiout, sinphiin, sinphiout;
+        cosphiin = sinthetain==0.f?1.f:clamp(wincident->x/sinthetain,-1.f,1.f);
+        cosphiout = sinthetaout==0.f?1.f:clamp(wout->x/sinthetaout,-1.f,1.f);
+        sinphiin = sqrtf(1.f - cosphiin * cosphiin);
+        sinphiout = sqrtf(1.f - cosphiout * cosphiout);
+        maxcos = max(0.0f, cosphiin * cosphiout + sinphiin * sinphiout);
+    }
+
+    float sinalpha, tanbeta;
+    if(abscosthetain>abscosthetaout)
+    {
+        sinalpha = sinthetaout;
+        tanbeta = sinthetain/abscosthetain;
+    }
+    else
+    {
+        sinalpha = sinthetain;
+        tanbeta = sinthetaout/abscosthetaout;
+    }
+
+    //diffuse/pi * (A+B*max[0,cos(phiin-phiout)]*sinalpha*tanbeta)
+    return Color(diffuse.r * INV_PI * (A+B*maxcos*sinalpha*tanbeta),
+                 diffuse.g * INV_PI * (A+B*maxcos*sinalpha*tanbeta),
+                 diffuse.b * INV_PI * (A+B*maxcos*sinalpha*tanbeta));
+}
---
+++ src/materials/oren_nayar.hpp
@@ -0,0 +1,23 @@
+
+#ifndef __OREN_NAYAR_HPP__
+#define __OREN_NAYAR_HPP__
+
+
+#include "bdf.hpp"
+#include "color.hpp"
+
+class OrenNayar : public Bdf
+{
+public:
+    OrenNayar(Color diffuse, float sigma);
+    ~OrenNayar();
+    Color df(const Vec3* wout, const Vec3* wincident)const;
+
+private:
+    Color diffuse;
+    float A;
+    float B;
+};
+
+
+#endif
