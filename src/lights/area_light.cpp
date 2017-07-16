5d70c802b7892a8ccb9031b05506f791e09641cd
Davide Pizzolotto <davide.pizzolotto@studenti.unitn.it>
2017-07-16 20:02:16 +0200

    Forgot to return spectrum in arealight radiance

--- src/lights/area_light.cpp
+++ src/lights/area_light.cpp
@@ -42,6 +42,7 @@ Color AreaLight::radiance(float r0, float r1, float r2, float r3,
         out->direction *= -1.f;
 
     *pdf = AreaLight::invarea * INV_TWOPI;
+    return AreaLight::c;
 }
 
 float AreaLight::pdf() const
