541f54685a3767138b20632ffdc143336a91ded6
Davide Pizzolotto <davide.pizzolotto@studenti.unitn.it>
2017-09-22 02:29:19 +0200

    Readded Fresnel conditions
    Fresnel conditions was removed because previously merged into Reflection
    and Refraction classes in fa5332fb619194538264ac8e4f305acee7754250 and
    02dc92cbea32e33df2b49f146ab4c0ab2cca6d64. Since the Cook-Torrance model
    contains the Fresnel term and the class was already written, it is now
    resuscitated and will be used in the previously mentioned microfacet
    model

---
+++ src/materials/fresnel_conditions.hpp
@@ -0,0 +1,133 @@
+//Created,   9 Jun 2017
+//Last Edit 22 Sep 2017
+
+/**
+ *  \file fresnel_conditions.hpp
+ *  \brief Implementation of the fresnel equations
+ *  \author    Davide Pizzolotto
+ *  \version   0.1
+ *  \date      22 Sep 2017
+ *  \copyright GNU GPLv3
+ */
+
+
+#ifndef __FRESNEL_CONDITIONS_HPP__
+#define __FRESNEL_CONDITIONS_HPP__
+
+#include "utility/spectrum.hpp"
+#include "utility/utility.hpp"
+
+/**
+ *  \class FresnelConditions fresnel_conditions.hpp
+ *  \brief Fresnel conditions interface
+ *
+ *  Abstract class for the two types of fresnel equations: Conductor and
+ *  Dielectric
+ */
+class FresnelConditions
+{
+public:
+
+    /** \brief Evaluate the incident power reflected or transmitted
+     *
+     *  Given an angle, determines how much of the incoming light is reflected
+     *  or transmitted
+     *
+     *  \param[in] cosin The angle between the two rays
+     *  \return The amount of light reflected or transmitted
+     */
+    virtual Spectrum eval(float cosin)const = 0;
+    
+    //No need for virtual destructor...
+    ///Default destructor
+    virtual ~FresnelConditions();
+};
+
+
+/**
+ *  \class Conductor fresnel_conditions.hpp "materials/fresnel_conditions.hpp"
+ *  \brief Fresnel equations for a conductive material
+ *
+ *  A class used to calculate the value of the fresnel equations in a conductive
+ *  material, thus considering the absorbed light turned into heat
+ */
+class Conductor : public FresnelConditions
+{
+public:
+
+    /** \brief Default constructor
+     *
+     *  Construct a conductive material with the given refraction index and
+     *  absorption. Despite the fact that conductive materials do not transmit
+     *  light, they absorb it, hence the needs for the index of refraction
+     *
+     *  \param[in] refraction Refraction index for the material
+     *  \param[in] absorption The portion of spectrum absorbed
+     */
+    Conductor(const Spectrum& refraction, const Spectrum& absorption);
+
+    /** \brief Evaluate the incident power reflected
+     *
+     *  Given an angle, determines how much of the incoming light is reflected,
+     *  removing the one absorbed and turned into heat
+     *
+     *  \param[in] cosin The angle between the two rays
+     *  \return The amount of light reflected
+     */
+    Spectrum eval(float cosin)const;
+
+private:
+
+    // ior
+    Spectrum refraction_index;
+
+    //absorbed spectrum
+    Spectrum absorption;
+};
+
+
+/**
+ *  \class Dielectric fresnel_conditions.hpp "materials/fresnel_conditions.hpp"
+ *  \brief Fresnel equations for a dielectric material
+ *
+ *  A class used to calculate the value of the fresnel equations in a dielectric
+ *  material
+ */
+class Dielectric : public FresnelConditions
+{
+public:
+
+    /** \brief Default constructor
+     *
+     *  Construct a dielectric material with the given refraction index for
+     *  incident and transmitted materials
+     *
+     *  \param[in] etai Refraction index for the incident ray
+     *  \param[in] etat Refraction index for the transmitted ray
+     */
+    Dielectric(const Spectrum& etai, const Spectrum& etat);
+
+    /** \brief Evaluate the incident power reflected or transmitted
+     *
+     *  Given an angle, determines how much of the incoming light is reflected.
+     *  In case the ray is transmitted, return the amount of incoming light
+     *  reflected, so the remaining of the original spectrum is the potion of
+     *  light transmitted.
+     *
+     *  \note This amount is calculated only for the parameter set by \p chosen
+     *
+     *  \param[in] cosin The angle between the two rays
+     *  \return The amount of light reflected, depending on the angle
+     */
+    Spectrum eval(float cosin)const;
+
+private:
+
+    //ior incident
+    float eta_i;
+
+    //ior transmitted
+    float eta_t;
+};
+
+#endif
