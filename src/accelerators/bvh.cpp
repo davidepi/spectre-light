davidepi/spectre-light

commit bbc58fe9a9259dc0f1e278f5150bf548e70c55fb

Multi materials per mesh support (Mesh/Triangle level)
Implemented a new sytem able to represent with little memory consumption
a per-triangle material assignation. However this has been implemented
only at Mesh and Triangle level, now this system should be inserted into
the Bvh/Kd-tree routines and made compatible with the HitPoint class and
Asset::getMaterial() function

src/primitives/mesh.cpp
@@ -3,18 +3,24 @@
 
 #include "mesh.hpp"
 
-Mesh::Mesh(unsigned int size)
+Mesh::Mesh(unsigned int size, unsigned char mat_size)
 {
     Mesh::count=0; //number of triangles added
     Mesh::alloc=size; //number of triangles allocated
 
     //can't use malloc, vtable pointer would be uninitialized
     Mesh::tris = new Triangle[size];
+    Mesh::materials = new const Bsdf*[mat_size];
+    //ensure at least the default material
+    if(mat_size<1)
+        mat_size=1;
+    Mesh::materials[0] = MtlLib.get("Default");
 }
 
 Mesh::~Mesh()
 {
-   delete[] Mesh::tris;
+    delete[] Mesh::tris;
+    delete[] Mesh::materials;
 }
 
 void Mesh::add_triangle(const Point3& a, const Point3& b, const Point3& c,
@@ -211,4 +217,9 @@ void Mesh::sample_point(float r0, float r1, const float* densities, Point3* p,
     sample01 = inverse_lerp(extSample-densities[mid], 0,
                             densities[mid+1]-densities[mid]);
     tris[mid].sample_point(sample01, r1, NULL, p, n);
+}
+
+void Mesh::set_bsdf(unsigned char index, const Bsdf* material)
+{
+    Mesh::materials[index] = material;
 }

src/primitives/mesh.hpp
@@ -1,5 +1,5 @@
 //Created,   6 Aug 2017
-//Last Edit 15 Dec 2017
+//Last Edit 28 Mar 2018
 
 /**
  *  \file mesh.hpp
@@ -7,7 +7,7 @@
  *  \details   All the methods to represent a triangle mesh in the space
  *  \author    Davide Pizzolotto
  *  \version   0.1
- *  \date      15 Dec 2017
+ *  \date      28 Mar 2018
  *  \copyright GNU GPLv3
  */
 
@@ -24,6 +24,8 @@
 #include "geometry/AABB.hpp"
 #include "utility/console.hpp"
 #include "accelerators/bvh.hpp"
+#include "materials/bdf.hpp"
+#include "materials/material_library.hpp"
 
 
 /**
@@ -44,8 +46,9 @@ public:
     /** \brief Default constructor
      *
      *  \param[in] size The number of triangles of the mesh
+     *  \param[in] mat_size The number of materials of the mesh. Immutable
      */
-    Mesh(unsigned int size);
+    Mesh(unsigned int size, unsigned char mat_size);
 
     ///Default destructor
     ~Mesh();
@@ -187,11 +190,49 @@ public:
      */
     void sample_point(float r, float r1, const float* densities, Point3* p,
                         Normal* n)const;
+    
+    /** \brief Add a material to the set of materials used in this Mesh
+     *
+     *  More than the usage of this function it is important to understand the
+     *  reason of it.
+     *  - Storing the material name for every triangle inside the Mesh and then
+     *    perform a lookup in the hash map at each intersection would be too
+     *    much computationally and memory expensive
+     *  - Storing directly a pointer to the material inside each triangle would
+     *    be too much computationally expensive (8KB every 1K triangles in
+     *    64-bit systems)
+     *
+     *  These problems are solved by keeping every material used in the Mesh
+     *  into an array. Every triangle stores an unsigned char that acts as an
+     *  offset of this array to find the corresponding material. With this
+     *  method 1KB of data is used every 1K triangles + the actual size of the
+     *  pointers (from 16 byte for a single material mesh up to 2K for mesh with
+     *  255 materials)
+     *
+     *  This method is used to set the material at the ith index to be
+     *  referenced by the triangles contained into the mesh. The 0th material
+     *  should be the Default material since every Triangle without a material
+     *  is assigned the index 0.
+     *
+     *  \warning This method does not allocate a bigger array if the index is
+     *  outside the referenced area. The parser should take care of not writing
+     *  outside the array bounds
+     *
+     *  \param[in] index The index at which the material is assigned. Index 0
+     *  represents the default material
+     *  \param[in] material A pointer to the material assigned to the given
+     *  index
+     */
+    void set_bsdf(unsigned char index, const Bsdf* material);
 
 private:
 
     //array of triangles
     Triangle* tris;
+    
+    //materials used in the Mesh (since storing a pointer for
+    //each face would be way too memory expensive)
+    const Bsdf** materials;
 
     //precomputed AABB
     AABB aabb;

src/primitives/triangle.cpp
@@ -6,7 +6,14 @@
 Triangle::Triangle(const Vertex& v0, const Vertex& v1, const Vertex& v2)
         :a(v0), b(v1), c(v2)
 {
+    Triangle::material_index = 0;
+}
 
+Triangle::Triangle(const Vertex& v0, const Vertex& v1, const Vertex& v2,
+                   unsigned char material_index)
+:a(v0), b(v1), c(v2)
+{
+    Triangle::material_index = material_index;
 }
 
 AABB Triangle::compute_AABB()const

src/primitives/triangle.hpp
@@ -62,13 +62,29 @@ public:
 
     /** \brief Constructor, given the vertices.
      *
-     *  Construct a triangle in object space with the given vertices
+     *  Construct a triangle in object space with the given vertices.
+     *  The material of this triangle is set as Default material
      *
      *  \param[in] v0 The first vertex of the triangle
      *  \param[in] v1 The second vertex of the triangle
      *  \param[in] v2 The third vertex of the triangle
      */
     Triangle(const Vertex& v0, const Vertex& v1, const Vertex& v2);
+    
+    /** \brief Constructor, given the vertices and the material.
+     *
+     *  Construct a triangle in object space with the given vertices and
+     *  material
+     *
+     *  \param[in] v0 The first vertex of the triangle
+     *  \param[in] v1 The second vertex of the triangle
+     *  \param[in] v2 The third vertex of the triangle
+     *  \param[in] material_index The index of the material used by this
+     *  Triangle, referencing the material array of the containing Mesh. More
+     *  info can be found in the Mesh::set_bsdf method
+     */
+    Triangle(const Vertex& v0, const Vertex& v1, const Vertex& v2,
+             unsigned char material_index);
 
     /** \brief Intersection of a Ray and this triangle
      *
@@ -155,6 +171,9 @@ public:
 
     ///Third vertex of the triangle
     Vertex c;
+    
+    ///Index of the corresponding material in the materials array of the Mesh
+    unsigned char material_index;
 };
 
 #endif

src/materials/material_library.hpp
@@ -28,8 +28,8 @@
  *  the various shape at creation time. The library must be unique and
  *  accessible everywhere.
  *
- *  The library contains a default material that represents a white matte
- *  surface
+ *  The library contains a default material called "Default" that represents a
+ *  white matte surface
  *
  *  A define grants access to this singleton just by writing "MtlLib"
  *

src/parsers/obj_parser.cpp
@@ -59,7 +59,7 @@ Mesh* parseObj(const char* path)
         if(tris>0)
         {
             fseek(fin, 0, SEEK_SET); //reposition to beginning
-            retval = new Mesh(tris);
+            retval = new Mesh(tris,1);
             Vertex a,b,c;
             char* token;
             char* pos;

src/parsers/obj_parser.y
@@ -2,26 +2,16 @@
 /* Last Edit 24 Mar 2018 */
 
 %{
-    #include <stdio.h>
-    #include <stdlib.h>
+    #include <cstdio>
+    #include <cstdlib>
+    #include <vector>
+    #include <geometry/vec3.hpp>
+    #include <utility/console.hpp>
     
-    int yylex();
-    int yyparse();
-    FILE *yyin;
-    
-    void yyerror(const char *s);
-    void objparser_set_facedata(int, int, int);
-    extern int yylineno;
-    int objparser_current_face = 0;
-    typedef struct
-    {
-        int v;
-        int vt;
-        int vn;
-    }objparser_face_t;
-    objparser_face_t objparser_face_data[3];
+    std::vector<Vec3> vertices;
 %}
 %error-verbose
+%define api.prefix {obj}
 //type that could be returned by flex
 %union
 {
@@ -41,6 +31,7 @@
 %token MTLFILE "material .mtl file path"
 %token USEMTL "material name"
 %token SEPARATOR " / separator"
+%token ENDOFFILE "end of file"
 
 %type <fval> float_num
 
@@ -48,13 +39,13 @@
 
 file:
 file stmt
-|
+| ENDOFFILE
 ;
 
 stmt:
-FACE face_point face_point face_point   {}
+FACE face_point face_point face_point
 | TEXTURE float_num float_num {printf("Texture: %f %f\n",$2,$3);}
-| VERTEX float_num float_num float_num {printf("Vertex: %f %f %f\n",$2,$3,$4);}
+| VERTEX float_num float_num float_num {vertices.push_back(Vec3($2,$3,$4));}
 | NORMAL float_num float_num float_num {printf("Normal: %f %f %f\n",$2,$3,$4);}
 | USEMTL STRING         {fprintf(stdout,"Use material %s\n",$2);}
 | MTLFILE STRING        {fprintf(stdout,"Parse material %s\n",$2);}
@@ -67,56 +58,15 @@ INT     {$$ = (float)$1;}
 ;
 
 face_point:
-INT                               { objparser_set_facedata($1,-1,-1); }
-| INT SEPARATOR INT               { objparser_set_facedata($1,$3,-1); }
-| INT SEPARATOR SEPARATOR INT     { objparser_set_facedata($1,-1,$4); }
-| INT SEPARATOR INT SEPARATOR INT { objparser_set_facedata($1,$3,$5); }
+INT
+| INT SEPARATOR INT
+| INT SEPARATOR SEPARATOR INT
+| INT SEPARATOR INT SEPARATOR INT
 ;
 
 %%
 
-int main(int argc, char* argv[])
-{
-    FILE *myfile = fopen(argv[1], "r");
-    if(!myfile)
-    {
-        fprintf(stderr,"Error opening file\n");
-        return -1;
-    }
-    yyin = myfile;
-    
-    do
-    {
-        yyparse();
-    }
-    while(!feof(yyin));
-}
-
-void yyerror(const char *s) {
-    fflush(stdout);
-    fprintf(stderr,"Error at line %d: %s\n",yylineno,s);
+void objFlexLexer::error(const char *s) {
+    Console.critical(s);
     exit(EXIT_FAILURE);
-}
-
-void objparser_set_facedata(int v, int vt, int vn)
-{
-    objparser_face_data[objparser_current_face].v = v;
-    objparser_face_data[objparser_current_face].vt = vt;
-    objparser_face_data[objparser_current_face].vn = vn;
-    objparser_current_face++;
-    if(objparser_current_face>2)
-    {
-        printf("Face: %d/%d/%d %d/%d/%d %d/%d/%d\n",
-        objparser_face_data[0].v,
-        objparser_face_data[0].vt,
-        objparser_face_data[0].vn,
-        objparser_face_data[1].v,
-        objparser_face_data[1].vt,
-        objparser_face_data[1].vn,
-        objparser_face_data[2].v,
-        objparser_face_data[2].vt,
-        objparser_face_data[2].vn
-        );
-        objparser_current_face = 0;
-    }
 }

tests/console_tests.cpp
@@ -62,6 +62,14 @@ TEST(Consolet,notice_boolean)
     errors_count[NOTICE_INDEX] = 0;
 }
 
+TEST(Consolet,notice_vargs)
+{
+    errors_count[NOTICE_INDEX] = 0;
+    Console.notice("test #%d %s",1,"ok");
+    EXPECT_EQ(errors_count[NOTICE_INDEX], 1);
+    errors_count[NOTICE_INDEX] = 0;
+}
+
 TEST(Consolet,warning)
 {
     errors_count[WARNING_INDEX] = 0;
@@ -80,6 +88,14 @@ TEST(Consolet,warning_boolean)
     errors_count[WARNING_INDEX] = 0;
     Console.warning(false,"test");
     EXPECT_EQ(errors_count[WARNING_INDEX], 0);
+    errors_count[WARNING_INDEX] = 0;
+}
+
+TEST(Consolet,warning_vargs)
+{
+    errors_count[WARNING_INDEX] = 0;
+    Console.warning("test #%d %s",1,"ok");
+    EXPECT_EQ(errors_count[WARNING_INDEX], 1);
     errors_count[WARNING_INDEX] = 0;
 }
 
@@ -104,6 +120,14 @@ TEST(Consolet,severe_boolean)
     errors_count[ERROR_INDEX] = 0;
 }
 
+TEST(Consolet,severe_vargs)
+{
+    errors_count[ERROR_INDEX] = 0;
+    Console.severe("test #%d %s",1,"ok");
+    EXPECT_EQ(errors_count[ERROR_INDEX], 1);
+    errors_count[ERROR_INDEX] = 0;
+}
+
 TEST(Consolet,critical)
 {
     errors_count[CRITICAL_INDEX] = 0;
@@ -122,6 +146,14 @@ TEST(Consolet,critical_boolean)
     errors_count[CRITICAL_INDEX] = 0;
     Console.critical(false,"test");
     EXPECT_EQ(errors_count[CRITICAL_INDEX], 0);
+    errors_count[CRITICAL_INDEX] = 0;
+}
+
+TEST(Consolet,critical_vargs)
+{
+    errors_count[CRITICAL_INDEX] = 0;
+    Console.critical("test #%d %s",1,"ok");
+    EXPECT_EQ(errors_count[CRITICAL_INDEX], 1);
     errors_count[CRITICAL_INDEX] = 0;
 }
 

commit 1e0daff015f1a122e25b714ffde129e580604837

Commit bbc58fe was garbage
Commit bbc58fe9a9259dc0f1e278f5150bf548e70c55fb broke the instancing
between shapes and thus it has been removed. Now it is implemented at
asset level, however in the current situation the Scene::add_asset must
be fixed to avoid segfault. To fix the add_asset the obj parser,
however, must be finished

src/accelerators/bvh.cpp
@@ -418,8 +418,12 @@ bool Bvh::intersect(const Ray* r, float* distance, HitPoint* h)const
             {
                 for(int i=0;i<node->len;i++)
                 {
-                    if(Bvh::tris[node->offset+i].intersect(r,distance,h))
+                    unsigned int tmp_index = node->offset+i;
+                    if(Bvh::tris[tmp_index].intersect(r,distance,h))
+                    {
+                        h->index = tmp_index;
                         found = true;
+                    }
                 }
                 if (jobs_stack_top > 0)
                     node = jobs[--jobs_stack_top];

src/integrators/path_tracer.cpp
@@ -24,7 +24,7 @@ Spectrum PathTracer::l_rec(const Scene *sc, const HitPoint *hp, const Ray *r,
         retval+=*power*((AreaLight *)hp->asset_h)->emissive_spectrum();
     
     //calculate direct lighting at point
-    const Bsdf* mat = hp->asset_h->get_material();
+    const Bsdf* mat = hp->asset_h->get_material(hp->index);
     Spectrum direct = direct_l(sc,hp,r,sam,ot);
     retval+=*power*direct;
     

src/integrators/ray_tracer.cpp
@@ -21,7 +21,7 @@ Spectrum direct_l(const Scene* sc, const HitPoint* hp, const Ray* r,
         //choose a light to sample
         int sampledlight = min((int)(rand[0]*nlights),nlights-1);
         const AreaLight* light = lights[sampledlight];
-        const Bsdf* mat = hp->asset_h->get_material();
+        const Bsdf* mat = hp->asset_h->get_material(hp->index);
         float lightpdf;
         float bsdfpdf;
         bool matchedSpec;

src/lights/area_light.cpp
@@ -4,15 +4,13 @@
 #include "area_light.hpp"
 
 AreaLight::AreaLight(const Shape* sp,const Matrix4& obj2World,const Spectrum& c)
-: Asset(sp,obj2World), c(c)
+: Asset(sp,obj2World,0), c(c)
 {
     //calculate the surface of the world-space object
     AreaLight::area = sp->surface(&obj2World);
-
     //get the cumulative densities of the various faces of the light
     cd = (float*)malloc(sizeof(float)*sp->get_faces_number());
     sp->get_densities_array(&obj2World, cd);
-
     AreaLight::invarea = 1.f/area;
 }
 

src/lights/area_light.hpp
@@ -1,12 +1,12 @@
 //Created,  13 Jun 2017
-//Last Edit 23 Jan 2018
+//Last Edit 30 Mar 2018
 
 /**
  *  \file area_light.hpp
  *  \brief     Definition of a light-emitting shape
  *  \author    Davide Pizzolotto
  *  \version   0.1
- *  \date      23 Jan 2018
+ *  \date      30 Mar 2018
  *  \copyright GNU GPLv3
  */
 

src/primitives/asset.cpp
@@ -4,13 +4,15 @@
 #include "asset.hpp"
 unsigned static int _asset_ID_pool = 1;
 
-Asset::Asset(const Shape* sp, const Matrix4& transform)
+Asset::Asset(const Shape* sp, const Matrix4& transform, unsigned char mat_no)
         :objToWorld(transform),id(_asset_ID_pool++),
          aabb(sp->compute_AABB(&transform))
 
 {
     Asset::model = sp;
-    Asset::material = MtlLib.get("Default");
+    Asset::
+    Asset::materials = (const Bsdf**)malloc(sizeof(Bsdf*)*(mat_no+1));
+    Asset::material_index = (unsigned char*)malloc(sp->get_faces_number());
     Asset::objToWorld.inverse(&(Asset::worldToObj));
 }
 
@@ -51,17 +53,17 @@ const AABB* Asset::get_AABB()const
     return &(Asset::aabb);
 }
 
-void Asset::set_material(const Bsdf *material)
-{
-    Asset::material = material;
-}
-
-const Bsdf* Asset::get_material() const
-{
-    return Asset::material;
-}
-
 bool Asset::is_light() const
 {
     return false;
+}
+
+void Asset::set_material(const Bsdf* material, unsigned char index)
+{
+    Asset::materials[index] = material;
+}
+
+const Bsdf* Asset::get_material(unsigned int index)const
+{
+    return Asset::materials[index];
 }

src/primitives/asset.hpp
@@ -1,5 +1,5 @@
 //Created,   6 May 2017
-//Last Edit 16 Dec 2017
+//Last Edit 30 Mar 2018
 
 /**
  *  \file asset.hpp
@@ -8,7 +8,7 @@
  *             materials and textures
  *  \author    Davide Pizzolotto
  *  \version   0.2
- *  \date      16 Dec 2017
+ *  \date      30 Mar 2018
  *  \copyright GNU GPLv3
  */
 
@@ -37,8 +37,9 @@ public:
      *  \param[in] sp A pointer to the Shape wrapped in this class
      *  \param[in] transform The transformation matrix that will be applied to
      *  the shape
+     *  \param[in] material_no The number of materials used in the Asset
      */
-    Asset(const Shape* sp, const Matrix4& transform);
+    Asset(const Shape* sp, const Matrix4& transform, unsigned char material_no);
 
     ///Default destructor
     virtual ~Asset() = default;
@@ -83,29 +84,57 @@ public:
      */
     const AABB* get_AABB() const;
 
-    /** \brief Set the material for the current asset
-     *
-     *  Set the material for the current asset. Note that the pointer will not
-     *  be owned by the asset
-     *
-     *  \param[in] material The pointer of the material that will be associated
-     *  with the current asset
-     */
-    void set_material(const Bsdf* material);
-
-    /** \brief Return a pointer to the material
-     *
-     *  Return the material associated with the asset
-     *
-     *  \return material A pointer to the material of the asset
-     */
-    const Bsdf* get_material()const;
-
     /** \brief Return true if this asset is a light
      *
      *  \return true if the asset is a light
      */
     virtual bool is_light()const;
+    
+    /** \brief Add a material to the set of materials used in this Asset
+     *
+     *  More than the usage of this function it is important to understand the
+     *  reason of it. In particular when considering Meshes where every triangle
+     *  has a different material. In this case:
+     *  - Storing the material name for every triangle inside the Mesh and then
+     *    perform a lookup in the hash map at each intersection would be too
+     *    much computationally and memory expensive
+     *  - Storing directly a pointer to the material inside each triangle would
+     *    be too much memory expensive (8KB every 1K triangles in 64-bits
+     *    systems)
+     *  Moreover, both the previous solutions break instancing.
+     *
+     *  These problems are solved by keeping every material used in the Mesh
+     *  into an array. Another array stores an unsigned char that acts as an
+     *  offset of this array to find the corresponding material. With this
+     *  method 1KB of data is used every 1K triangles + the actual size of the
+     *  pointers (from 16 byte for a single material mesh up to 2K for mesh with
+     *  255 materials)
+     *
+     *  This method is used to set the material at the ith index to be
+     *  referenced by the triangles contained into the mesh. The 0th material
+     *  should be the Default material since every Triangle without a material
+     *  is assigned the index 0.
+     *
+     *  \warning This method does not allocate a bigger array if the index is
+     *  outside the referenced area. The parser should take care of not writing
+     *  outside the array bounds
+     *
+     *  \param[in] index The index at which the material is assigned. Index 0
+     *  represents the default material
+     *  \param[in] material A pointer to the material assigned to the given
+     *  index
+     */
+    void set_material(const Bsdf* material, unsigned char index);
+    
+    /** \brief Return a pointer to the material
+     *
+     *  Return the material associated with the asset or the particular triangle
+     *
+     *  \param[in] index The ith face of the mesh for which the material should
+     *  be looked up. 0 Returns the default material
+     *  \return material A pointer to the material of the asset
+     */
+    const Bsdf* get_material(unsigned int index)const;
 
 protected:
 
@@ -126,8 +155,11 @@ private:
     //Bounding box
     AABB aabb;
 
-    //Material
-    const Bsdf* material;
+    //Materials
+    const Bsdf** materials;
+    
+    //index associating every face to every material
+    unsigned char* material_index;
 
 };
 

src/primitives/box.cpp
@@ -121,6 +121,7 @@ bool Box::intersect(const Ray* r,float* distance,HitPoint* h)const
         h->right = Vec3(h->normal_h.z,0,0);
     else
         h->right = Vec3(-h->normal_h.y,h->normal_h.x,0);
+    h->index = 1;
     return true;
 }
 

src/primitives/mesh.cpp
@@ -10,17 +10,11 @@ Mesh::Mesh(unsigned int size, unsigned char mat_size)
 
     //can't use malloc, vtable pointer would be uninitialized
     Mesh::tris = new Triangle[size];
-    Mesh::materials = new const Bsdf*[mat_size];
-    //ensure at least the default material
-    if(mat_size<1)
-        mat_size=1;
-    Mesh::materials[0] = MtlLib.get("Default");
 }
 
 Mesh::~Mesh()
 {
     delete[] Mesh::tris;
-    delete[] Mesh::materials;
 }
 
 void Mesh::add_triangle(const Point3& a, const Point3& b, const Point3& c,
@@ -217,9 +211,4 @@ void Mesh::sample_point(float r0, float r1, const float* densities, Point3* p,
     sample01 = inverse_lerp(extSample-densities[mid], 0,
                             densities[mid+1]-densities[mid]);
     tris[mid].sample_point(sample01, r1, NULL, p, n);
-}
-
-void Mesh::set_bsdf(unsigned char index, const Bsdf* material)
-{
-    Mesh::materials[index] = material;
 }

src/primitives/mesh.hpp
@@ -1,5 +1,5 @@
 //Created,   6 Aug 2017
-//Last Edit 28 Mar 2018
+//Last Edit 30 Mar 2018
 
 /**
  *  \file mesh.hpp
@@ -7,7 +7,7 @@
  *  \details   All the methods to represent a triangle mesh in the space
  *  \author    Davide Pizzolotto
  *  \version   0.1
- *  \date      28 Mar 2018
+ *  \date      30 Mar 2018
  *  \copyright GNU GPLv3
  */
 
@@ -190,49 +190,11 @@ public:
      */
     void sample_point(float r, float r1, const float* densities, Point3* p,
                         Normal* n)const;
-    
-    /** \brief Add a material to the set of materials used in this Mesh
-     *
-     *  More than the usage of this function it is important to understand the
-     *  reason of it.
-     *  - Storing the material name for every triangle inside the Mesh and then
-     *    perform a lookup in the hash map at each intersection would be too
-     *    much computationally and memory expensive
-     *  - Storing directly a pointer to the material inside each triangle would
-     *    be too much computationally expensive (8KB every 1K triangles in
-     *    64-bit systems)
-     *
-     *  These problems are solved by keeping every material used in the Mesh
-     *  into an array. Every triangle stores an unsigned char that acts as an
-     *  offset of this array to find the corresponding material. With this
-     *  method 1KB of data is used every 1K triangles + the actual size of the
-     *  pointers (from 16 byte for a single material mesh up to 2K for mesh with
-     *  255 materials)
-     *
-     *  This method is used to set the material at the ith index to be
-     *  referenced by the triangles contained into the mesh. The 0th material
-     *  should be the Default material since every Triangle without a material
-     *  is assigned the index 0.
-     *
-     *  \warning This method does not allocate a bigger array if the index is
-     *  outside the referenced area. The parser should take care of not writing
-     *  outside the array bounds
-     *
-     *  \param[in] index The index at which the material is assigned. Index 0
-     *  represents the default material
-     *  \param[in] material A pointer to the material assigned to the given
-     *  index
-     */
-    void set_bsdf(unsigned char index, const Bsdf* material);
 
 private:
 
     //array of triangles
     Triangle* tris;
-    
-    //materials used in the Mesh (since storing a pointer for
-    //each face would be way too memory expensive)
-    const Bsdf** materials;
 
     //precomputed AABB
     AABB aabb;

src/primitives/shape.hpp
@@ -1,5 +1,5 @@
 //Created,  25 Feb 2016
-//Last Edit 26 Nov 2017
+//Last Edit 30 Mar 2018
 
 /**
  *  \file shape.hpp
@@ -7,7 +7,7 @@
  *  \details   The superclass from which every shape inherits
  *  \author    Davide Pizzolotto
  *  \version   0.1
- *  \date      26 Nov 2017
+ *  \date      30 Mar 2018
  *  \copyright GNU GPLv3
  */
 
@@ -165,6 +165,9 @@ struct HitPoint
 
     ///Hit asset
     const Asset* asset_h;
+    
+    ///index of the hit triangle (1 if the asset is not a mesh)
+    unsigned int index;
     
     ///Mapping coordinate u of the point, used for texture mapping
     float u;

src/primitives/sphere.cpp
@@ -75,6 +75,7 @@ bool Sphere::intersect(const Ray* r,float* distance, HitPoint* hit)const
             hit->point_h.x = SELF_INTERSECT_ERROR; //-length vector
         }
         hit->right = Vec3(-TWO_PI * hit->point_h.y,TWO_PI * hit->point_h.x,0);
+        hit->index = 1;
         return true;
     }
     else

src/primitives/triangle.cpp
@@ -6,14 +6,7 @@
 Triangle::Triangle(const Vertex& v0, const Vertex& v1, const Vertex& v2)
         :a(v0), b(v1), c(v2)
 {
-    Triangle::material_index = 0;
-}
-
-Triangle::Triangle(const Vertex& v0, const Vertex& v1, const Vertex& v2,
-                   unsigned char material_index)
-:a(v0), b(v1), c(v2)
-{
-    Triangle::material_index = material_index;
+    
 }
 
 AABB Triangle::compute_AABB()const

src/primitives/triangle.hpp
@@ -171,9 +171,6 @@ public:
 
     ///Third vertex of the triangle
     Vertex c;
-    
-    ///Index of the corresponding material in the materials array of the Mesh
-    unsigned char material_index;
 };
 
 #endif

src/utility/scene.cpp
@@ -64,9 +64,9 @@ unsigned int Scene::add_asset(unsigned int shapeid, const Matrix4& transformMat,
                 return 0;
             }
         }
-        Asset* addme = new Asset(shapes[shapeid], transformMat);
+        Asset* addme = new Asset(shapes[shapeid], transformMat, 1);
         if(material!=NULL)
-            addme->set_material(material);
+            addme->set_material(material,1);
         Scene::assets[Scene::asset_index++] = addme;
         k.addAsset(addme); //add to kdtree
         return addme->get_id();